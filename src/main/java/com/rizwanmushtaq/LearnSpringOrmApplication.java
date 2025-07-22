d1c04a9ccfa02c9dbc5ef7d87b0b8b00073c8ae1
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-22 10:17:37 +0200

    added applcation configurations

--- src/main/java/com/rizwanmushtaq/LearnSpringOrmApplication.java
+++ src/main/java/com/rizwanmushtaq/LearnSpringOrmApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnSpringOrmApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringOrmApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LearnSpringOrmApplication.class, args);
+  }
 }
